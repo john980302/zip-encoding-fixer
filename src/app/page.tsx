efb169c9416ebf25a9a92836695750d1f30d122b
Choi Woojin <wjchoi@gomiro.com>
2026-02-04 10:06:19 +0900

    Refactor build script to remove webpack flag and update RootPage to use direct redirection to '/ko'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace('/ko');
-  }, [router]);
-
-  return null;
+  redirect('/ko/');
 }
